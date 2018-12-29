348e7b4593f8bd71565022e9af2e27edfcc7a07d
81klma1bif <81klma1bif@hft-stuttgart.de>
2018-12-29 20:26:53 +0100

    add Panel and Layout to  Highscores

--- src/de/pictastic/singlePong/Highscores.java
+++ src/de/pictastic/singlePong/Highscores.java
@@ -1,30 +1,42 @@
 package de.pictastic.singlePong;
 
+import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
  * displays the highscores list on this system after the game
  */
 public class Highscores extends JPanel implements ActionListener {
+	
 	private JButton start;
+	JPanel panel;
+	CardLayout cardLayout;
 
 	private ArrayList<Highscore> scores = new ArrayList<Highscore>();
 /**
  * 
+ * @param panel
+ * @param cardLayout
  * @param playedHighscore
  */
-	public Highscores(Highscore playedHighscore) {
+	public Highscores(JPanel panel, CardLayout cardLayout, Highscore playedHighscore) {
+		add(new JLabel("Higscores"));
+		this.cardLayout=cardLayout;
+		this.panel=panel;
+		
 		start = new JButton("Go Back to Start");
 		add(start);
 		start.addActionListener(this);
 
 	}
+
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -35,7 +47,6 @@ public class Highscores extends JPanel implements ActionListener {
 
 	/**
 	 * Fügt den Score zur Highscoreliste hinzu, wenn dieser in der Top10 ist.
-	 * Entfernt bei Bedarf den aus Platz 11. fallenden Score.
 	 * @param score
 	 */
 	public void addNewScore(Highscore score) {
