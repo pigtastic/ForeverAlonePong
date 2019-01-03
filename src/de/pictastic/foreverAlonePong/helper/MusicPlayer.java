580453a249d0e8bc49263afb061b18bef184267c
Marvin Klein <43863667+marvinxkl@users.noreply.github.com>
2019-01-03 12:25:45 +0100

    Create MusicPlayer.java

---
+++ src/de/pictastic/foreverAlonePong/helper/MusicPlayer.java
@@ -0,0 +1,26 @@
+package de.pictastic.foreverAlonePong.helper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+public class MusicPlayer {
+
+
+	private static InputStream input;
+
+	public static void playMusic(String path) {
+		try {
+			input = new FileInputStream(new File(path));
+			AudioStream audios = new AudioStream(input);
+			AudioPlayer.player.start(audios);
+
+		} catch (Exception e) { 
+			System.out.println("Wiedergabe fehlgeschlagen");
+			
+		}
+	}
+}
